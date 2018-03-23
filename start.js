477a644a47a2aa5e0d9d9cc1cc5fb81c90c2af69
Sander van t Einde <sander.vanteinde@infosupport.com>
2018-03-23 14:15:10 +0100

    re-enable http listener

--- start.js
+++ start.js
@@ -1,13 +1,34 @@
 const loop = require('./utils/loop');
 const config = require('./config');
+const server = require('./utils/server');
+const fs = require('fs');
 
 //load init directory
-const fs = require('fs');
 fs.readdir('./init', (err, files) => {
     files.forEach(file => {
         require(`./init/${file}`);
     });
 });
+
+server.startNormal(config.webPort);
+if(config.key && config.cert)
+{
+    let key = null;
+    let cert = null;
+    let onDone = () => {
+        if(key == null || cert == null) return;
+        server.startSecure({key,cert, port: config.port});
+    }
+    fs.readFile(config.key,{encoding: 'utf8'}, (err, data) => {
+        key = data;
+        onDone();
+    });
+    fs.readFile(config.cert, {encoding: 'utf8'}, (err, data)=>{
+        cert = data;
+        onDone();
+    });
+}
+
 
 //start the loop
 loop.run();
